4d7a25c946b9e2647a0f58dd3fb85c3da85d33f0
James Dyer <jamesdyer199@gmail.com>
2019-07-27 23:10:17 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## RequiemPowerBot
+
+A Reddit bot ([u/RequiemPowerBot](https://www.reddit.com/user/RequiemPowerBot/)) that breaks up comment chains of three or more of the same comment with a JoJo meme.
---
+++ bot.py
@@ -0,0 +1,29 @@
+import praw
+
+BOT_NAME = 'RequiemPowerBot'
+REPLY_MESSAGE = 'This is... the power of [Requiem](https://youtu.be/qs3t2pE4ZsE)'
+MIN_CHAIN_LEN = 3
+
+reddit = praw.Reddit(BOT_NAME)
+
+# Look for chains among strings of comments everywhere
+while True:
+    for comment in reddit.subreddit('all').stream.comments():
+        print(f'Looking at r/{comment.subreddit} comment: "{comment.body[:100].replace('\n', '')}..."')
+
+        # Check if the comment and its parents form a chain
+        original_comment = comment
+        is_chain = True
+        chain_len = 1
+        while chain_len < MIN_CHAIN_LEN:
+            parent = comment.parent()
+            if type(parent) != praw.models.Comment or parent.body != comment.body:
+                is_chain = False
+                break
+            chain_len += 1
+            comment = parent
+
+        # Reply and break the chain if found
+        if is_chain:
+            original_comment.reply(REPLY_MESSAGE)
+            print(f'--- !!! Replied to comment! !!! ---\n')
