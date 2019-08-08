e40ab5edc6c4e2c34f702452a2ee25d7edb3bfb9
James Dyer <jamesdyer199@gmail.com>
2019-08-08 23:40:21 +1000

    Remove disabled features

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
-import json
 import logging
-import os
-import time
 import threading
 
 import praw
@@ -14,11 +11,7 @@ REPLY_MESSAGE = 'This is... the power of [Requiem](https://youtu.be/qs3t2pE4ZsE?
 SPOILER_SAFE_MESSAGE = 'This is... the power of ^(*a JoJo Part 5 spoiler*) >![Requiem](https://youtu.be/qs3t2pE4ZsE?t=122)!<.'
 CHAIN_LEN = 5
 COMMENT_SUMMARY_LEN = 50
-DEFAULT_TARGET_SUBS = ('ShitPostCrusaders', 'Animemes', 'animememes')
-MULTIREDDITS = ('target_subs', 'ignored_subs')
-MAX_TARGETS = 100
-EXPAND_INTERVAL = 60 * 60 * 24  # 1 day in seconds
-PRUNE_INTERVAL = 60 * 10  # 10 minutes in seconds
+TARGET_SUBS = ('ShitPostCrusaders', 'Animemes', 'animememes')
 
 
 class RequiemPowerBot:
@@ -29,13 +22,8 @@ class RequiemPowerBot:
 
         self.reddit = praw.Reddit(BOT_NAME)
 
-        # Load multireddit data
-        for multi in MULTIREDDITS:
-            setattr(self, multi, self.reddit.multireddit(BOT_NAME, multi))
-
-        # Give the summon response and experimentation features to daemon threads
-        for f in (self.respond_to_summons,):  # note: expanding and pruning disabled
-            threading.Thread(target=f, daemon=True, name='Thread-' + f.__name__).start()
+        # Give the summon response feature to a daemon thread
+        threading.Thread(target=self.respond_to_summons, daemon=True, name='Thread-summons').start()
 
         # Set the main thread to work on looking to break comment chains
         self.break_chains()
@@ -77,50 +65,6 @@ class RequiemPowerBot:
                     comment = self.reddit.comment(msg.id)
                     self.reply_with_meme(comment)
                     msg.mark_read()
-
-    def expand_target_subs(self):
-        """ Occasionally attempt to expand to a new target sub. """
-
-        while True:
-            logger.info('Trying to expand target subs')
-            # Try out a random sub that we haven't ignored or been banned from
-            while True:
-                new_sub = self.reddit.random_subreddit()
-                logger.info(f'Got random sub: {new_sub}')
-                if not new_sub.user_is_banned and new_sub not in self.ignored_subs.subreddits:
-                    break
-            logger.info(f'Success! Adding {new_sub} to targets')
-            self.target_subs.add(new_sub)
-
-            time.sleep(EXPAND_INTERVAL)
-
-    def prune_target_subs(self):
-        """ Ignore subs where we have low comment karma. """
-
-        while True:
-            # Set a lower karma bound based on how many targets we have
-            if len(self.target_subs.subreddits) > MAX_TARGETS / 2:
-                min_karma = 2
-            else:
-                min_karma = 1
-
-            # Check for target subs we should ignore
-            karma_dict = self.reddit.user.karma()
-            for sub in self.target_subs.subreddits:
-                # Remove subreddits we're banned from
-                if sub.user_is_banned:
-                    self.target_subs.remove(sub)
-
-                # Ignore subs we have low karma in
-                elif sub in karma_dict and karma_dict[sub]['comment_karma'] < min_karma:
-                    logger.info(f'Ignoring {sub}')
-                    if len(self.ignored_subs.subreddits) >= MAX_TARGETS:
-                        logger.info('Too many ignored subs: resetting')
-                        self.ignored_subs.update(subreddits=[])
-                    self.target_subs.remove(sub)
-                    self.ignored_subs.add(sub)
-
-            time.sleep(PRUNE_INTERVAL)
 
     @staticmethod
     def reply_with_meme(comment):
